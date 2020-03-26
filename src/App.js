84f7d0cde780f225619444e8720a41f1bbeb7e81
MadSadLad <shivam.119966@yahoo.com>
2020-03-26 22:29:12 +0530

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./services/Login";
 import Registration from "./pages/register";
@@ -7,14 +7,20 @@ import FilePickerScreen from "./pages/FilePickerScreen";
 import FormBuilderScreen from "./pages/FormBuilderScreen";
 // FilePickerScreen and FormSchemaBuilder
 //Just to Show working, will be removed when work with the flow
-
+const NavBar = () => (
+  <div>
+    <Link to="/home">Home</Link>
+    <Link to="/registration">Register</Link>
+    <Link to="/">Login</Link>
+  </div>
+);
 function App() {
-
   return (
     <div>
       <h1>Pharaoh 🐈</h1>
       <Router>
         <div className="App">
+          <NavBar />
           <Switch>
             <Route path="/" exact component={Login} />
             <Route path="/home" exact component={Home} />
