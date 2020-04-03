ecdc59030ba3f08d3193a4599b1cf2a3e0272475
Harsh Bhatia <bhatiaharsh.007@gmail.com>
2020-04-03 18:08:57 +0530

    url modification

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,14 +5,12 @@ import VideoTemplates from "pages/VideoTemplates";
 import React from "react";
 import { NavLink, Route, Switch, useRouteMatch } from "react-router-dom";
 
-
 export default () => {
-
   let { path, url } = useRouteMatch();
 
   return (
-    <div>
-      <div>
+    <div style={{ display: "flex" }}>
+      <div style={{ display: "flex", flex: 1, flexDirection: "column" }}>
         <NavLink to={`${url}/profile`}>
           <h3 style={{ display: "inline" }}>Your Profile and Settings</h3>
         </NavLink>
@@ -24,11 +22,13 @@ export default () => {
         </NavLink>
       </div>
       <Switch>
-        <Route path={`${path}/`} exact component={Dashboard} />
-        <Route path={`${path}/profile`} component={Profile} />
-        <Route path={`${path}/videoTemplates`} component={VideoTemplates} />
-        <Route path={`${path}/orders`} component={Orders} />
+        <div style={{ flex: 3 }}>
+          <Route path={`${path}/`} exact component={Dashboard} />
+          <Route path={`${path}/profile`} component={Profile} />
+          <Route path={`${path}/videoTemplates`} component={VideoTemplates} />
+          <Route path={`${path}/orders`} component={Orders} />
+        </div>
       </Switch>
     </div>
   );
-}
+};
