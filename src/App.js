e832bcb26dfc570f05e19aaf1f70a270fa6338cb
Harsh Bhatia <bhatiaharsh.007@gmail.com>
2020-04-09 01:24:11 +0530

    Add bootstrap css :|

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import Navbar from "components/Navbar";
 import PrivateRoute from "components/PrivateRoute";
 import Home from "pages/Home";
@@ -7,7 +9,6 @@ import Register from "pages/Register";
 import React from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import useAuth from "services/auth";
-
 export default () => {
   const { logout, isAuthenticated } = useAuth();
 
