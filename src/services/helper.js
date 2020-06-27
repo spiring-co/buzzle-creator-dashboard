73de6a98a65b9e146d0c07b756e50e6f4f297b5c
AbdulBsit <abasit325@gmail.com>
2020-06-27 16:17:28 +0530

    removed unnecessary function in helper

--- src/services/helper.js
+++ src/services/helper.js
@@ -17,36 +17,4 @@ export function getLayersFromComposition(c, type) {
   } else return [];
 }
 
-export const jobSchemaConstructor = (template) => {
-  return template.versions.map((version) => {
-    return {
-      idVideoTemplate: template.id,
-      idVersion: version.id,
-
-      assets: (function () {
-        return version.editableLayers.map((layer) => {
-          switch (layer.type) {
-            case "data":
-              return {
-                type: layer.type,
-                value: layer.label,
-                layerName: layer.layerName,
-                property: "Source Text",
-              };
-            case "image":
-              return {
-                type: layer.type,
-                layerName: layer.layerName,
-                src: `https://dummyimage.com/${layer.width}x${layer.height}/0011ff/fff`,
-                extension: "png",
-              };
-            default:
-              return;
-          }
-        });
-      })(),
-    };
-  });
-};
-
 export const getUniqueId = () => Math.random().toString(36).substr(2, 9);
