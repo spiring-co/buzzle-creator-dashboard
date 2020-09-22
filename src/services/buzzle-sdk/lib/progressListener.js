154fb26799bed6fd7ecd32a2ba93a066d11a2416
AbdulBsit <abasit325@gmail.com>
2020-09-22 10:48:16 +0530

    updated

--- src/services/buzzle-sdk/lib/progressListener.js
+++
@@ -1,7 +0,0 @@
-const io = require("socket.io-client");
-
-module.exports = function (socketUrl) {
-  const socket = io.connect(socketUrl);
-  console.log("socket", socketUrl);
-  return socket;
-};
