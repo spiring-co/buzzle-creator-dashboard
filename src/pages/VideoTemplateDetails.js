c87c0d617480c3938b781e79813e36494d8c3f22
MadSadLad <shivam.119966@yahoo.com>
2020-10-22 12:55:51 +0530

    sample video undefined

--- src/pages/VideoTemplateDetails.js
+++ src/pages/VideoTemplateDetails.js
@@ -2,23 +2,26 @@ import {
   Button,
   LinearProgress,
   Paper,
-  Typography, Container, Box,
-  withStyles, Chip
+  Typography,
+  Container,
+  Box,
+  withStyles,
+  Chip,
 } from "@material-ui/core";
 import { Alert } from "@material-ui/lab";
 import { Job, VideoTemplate, Creator } from "services/api";
 import React, { useState, useEffect } from "react";
 import { useHistory, useParams, useRouteMatch } from "react-router-dom";
 import useApi from "services/apiHook";
-import { zipMaker } from "helpers/downloadTemplateHelper"
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
+import { zipMaker } from "helpers/downloadTemplateHelper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
 import formatTime from "helpers/formatTime";
-import { useCurrency } from "services/currencyContext"
+import { useCurrency } from "services/currencyContext";
 import RejectionReasonPrompt from "./RejectionReasonPrompt";
 import RoleBasedView from "components/RoleBasedView";
 
@@ -35,26 +38,26 @@ export default () => {
   const { url } = useRouteMatch();
   const { id } = useParams();
   const history = useHistory();
-  const { getConvertedCurrency } = useCurrency()
+  const { getConvertedCurrency } = useCurrency();
   const [isDeleting, setIsDeleting] = useState(false);
-  const [isLoading, setIsLoading] = useState(false)
-  const [isDialogOpen, setIsDialogOpen] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [error, setError] = useState(null);
-  const [data, setData] = useState({})
-  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
-    init()
-  }, [])
+    init();
+  }, []);
 
   const init = async () => {
     try {
-      setData(await VideoTemplate.get(id))
-      setLoading(false)
+      setData(await VideoTemplate.get(id));
+      setLoading(false);
     } catch (err) {
-      setLoading(false)
-      setError(err)
+      setLoading(false);
+      setError(err);
     }
-  }
+  };
 
   const handleDownload = async () => {
     setIsLoading(true);
@@ -82,7 +85,7 @@ export default () => {
 
   const StyledTableRow = withStyles((theme) => ({
     root: {
-      '&:nth-of-type(odd)': {
+      "&:nth-of-type(odd)": {
         backgroundColor: theme.palette.action.hover,
       },
     },
@@ -102,19 +105,24 @@ export default () => {
     }
   };
 
-
-  const handleUpdateTemplate = async (publishState = "unpublished", rejectionReason = "") => {
+  const handleUpdateTemplate = async (
+    publishState = "unpublished",
+    rejectionReason = ""
+  ) => {
     try {
-      setIsLoading(true)
-      await VideoTemplate.update(data?.id, { ...data, publishState, rejectionReason })
-      setIsLoading(false)
-      history.push('/home/videoTemplates')
+      setIsLoading(true);
+      await VideoTemplate.update(data?.id, {
+        ...data,
+        publishState,
+        rejectionReason,
+      });
+      setIsLoading(false);
+      history.push("/home/videoTemplates");
     } catch (err) {
-      setIsLoading(false)
-      setError(err)
+      setIsLoading(false);
+      setError(err);
     }
-  }
-
+  };
 
   return (
     <div>
@@ -122,73 +130,115 @@ export default () => {
       {error && <Alert severity="error" children={`${error.message}`} />}
       <Paper style={{ padding: 20 }}>
         <Container>
-          <video
-            id="sample"
-            controls={true}
-            style={{ width: 300, height: 200, marginTop: 10 }}
-            src={data?.versions ?? [][0]?.sample}
-          />
+          {(data?.versions ?? [])[0]?.sample === undefined ? (
+            <Typography>{"sample video not found"}</Typography>
+          ) : (
+            <video
+              id="sample"
+              controls={true}
+              style={{ width: 300, height: 200, marginTop: 10 }}
+              src={(data?.versions ?? [])[0]?.sample} 
+            />
+          )}
           <div>
             <Typography variant="h6">{data?.title}</Typography>
-            <Typography >{data?.description}</Typography>
+            <Typography>{data?.description}</Typography>
             <Box>
-              <Typography style={{ marginTop: 10, }}>
-                Publish State
-            </Typography>
+              <Typography style={{ marginTop: 10 }}>Publish State</Typography>
               <Chip
                 size="small"
-                label={data?.publishState?.toUpperCase() ?? 'UNPUBLISHED'}
+                label={data?.publishState?.toUpperCase() ?? "UNPUBLISHED"}
                 style={{
-                  background: getColorFromState(data?.publishState ?? 'unpublished'),
+                  background: getColorFromState(
+                    data?.publishState ?? "unpublished"
+                  ),
                   color: "white",
                 }}
               />
             </Box>
-            {data?.rejectionReason && <Box>
-              <Typography style={{ marginTop: 10, }}>
-                Reason
-            </Typography>
-              <Typography style={{ color: getColorFromState(data?.publishState ?? 'unpublished') }}>
-                {data?.rejectionReason}
-              </Typography>
-            </Box>}
-            <RoleBasedView allowedRoles={['Admin']}><Table size="small" aria-label="a dense table" style={{ marginTop: 20, marginBottom: 20 }}>
-              <TableHead>
-                <StyledTableRow>
-                  <StyledTableCell>Version Name</StyledTableCell>
-                  <StyledTableCell >Average Render Time</StyledTableCell>
-                  <StyledTableCell >Render Cost/min</StyledTableCell>
-                  <StyledTableCell >Render Cost</StyledTableCell>
-                  <StyledTableCell >Loyalty Amount</StyledTableCell>
-                </StyledTableRow>
-              </TableHead>
-              <TableBody>
-                {data?.versions?.map(({ id, title, averageRenderTime = "", loyaltyCurrency, loyaltyValue = 0.0 }) => (
-                  <StyledTableRow key={id}>
-                    <StyledTableCell >
-                      {title}
-                    </StyledTableCell>
-                    <StyledTableCell >{!averageRenderTime ? 'NA' : `${formatTime(averageRenderTime)}`}</StyledTableCell>
-                    {/* TODO  here comes the fixed cost /min for render */}
-                    <StyledTableCell>{getConvertedCurrency(5, 'INR', 'INR')}</StyledTableCell>
-                    {/* TODO  here comes the fixed cost /min for render instead of 5 */}
-                    <StyledTableCell>{getConvertedCurrency(getRenderCostForTemplate(averageRenderTime, 5).toFixed(2), 'INR', 'INR')}</StyledTableCell>
-                    <StyledTableCell >
-                      {`${loyaltyValue} ${loyaltyCurrency} ${loyaltyCurrency !== 'INR' ? `(${getConvertedCurrency(loyaltyValue, loyaltyCurrency, 'INR')})` : ''}`}
-                    </StyledTableCell>
+            {data?.rejectionReason && (
+              <Box>
+                <Typography style={{ marginTop: 10 }}>Reason</Typography>
+                <Typography
+                  style={{
+                    color: getColorFromState(
+                      data?.publishState ?? "unpublished"
+                    ),
+                  }}>
+                  {data?.rejectionReason}
+                </Typography>
+              </Box>
+            )}
+            <RoleBasedView allowedRoles={["Admin"]}>
+              <Table
+                size="small"
+                aria-label="a dense table"
+                style={{ marginTop: 20, marginBottom: 20 }}>
+                <TableHead>
+                  <StyledTableRow>
+                    <StyledTableCell>Version Name</StyledTableCell>
+                    <StyledTableCell>Average Render Time</StyledTableCell>
+                    <StyledTableCell>Render Cost/min</StyledTableCell>
+                    <StyledTableCell>Render Cost</StyledTableCell>
+                    <StyledTableCell>Loyalty Amount</StyledTableCell>
                   </StyledTableRow>
-                ))}
-              </TableBody>
-            </Table></RoleBasedView>
-            <RoleBasedView
-              allowedRoles={['Creator']}
-            ><Button
-              style={{ margin: 10, marginLeft: 0 }}
-              variant="contained"
-              color="primary"
-              onClick={handleEdit}>
+                </TableHead>
+                <TableBody>
+                  {data?.versions?.map(
+                    ({
+                      id,
+                      title,
+                      averageRenderTime = "",
+                      loyaltyCurrency,
+                      loyaltyValue = 0.0,
+                    }) => (
+                      <StyledTableRow key={id}>
+                        <StyledTableCell>{title}</StyledTableCell>
+                        <StyledTableCell>
+                          {!averageRenderTime
+                            ? "NA"
+                            : `${formatTime(averageRenderTime)}`}
+                        </StyledTableCell>
+                        {/* TODO  here comes the fixed cost /min for render */}
+                        <StyledTableCell>
+                          {getConvertedCurrency(5, "INR", "INR")}
+                        </StyledTableCell>
+                        {/* TODO  here comes the fixed cost /min for render instead of 5 */}
+                        <StyledTableCell>
+                          {getConvertedCurrency(
+                            getRenderCostForTemplate(
+                              averageRenderTime,
+                              5
+                            ).toFixed(2),
+                            "INR",
+                            "INR"
+                          )}
+                        </StyledTableCell>
+                        <StyledTableCell>
+                          {`${loyaltyValue} ${loyaltyCurrency} ${
+                            loyaltyCurrency !== "INR"
+                              ? `(${getConvertedCurrency(
+                                  loyaltyValue,
+                                  loyaltyCurrency,
+                                  "INR"
+                                )})`
+                              : ""
+                          }`}
+                        </StyledTableCell>
+                      </StyledTableRow>
+                    )
+                  )}
+                </TableBody>
+              </Table>
+            </RoleBasedView>
+            <RoleBasedView allowedRoles={["Creator"]}>
+              <Button
+                style={{ margin: 10, marginLeft: 0 }}
+                variant="contained"
+                color="primary"
+                onClick={handleEdit}>
                 Edit
-            </Button>
+              </Button>
               <Button
                 style={{ margin: 10, marginLeft: 0 }}
                 variant="contained"
@@ -197,7 +247,7 @@ export default () => {
                   history.push({
                     pathname: `${url}/publish`,
                     state: {
-                      videoTemplate: data
+                      videoTemplate: data,
                     },
                   });
                 }}>
@@ -211,14 +261,14 @@ export default () => {
                 color="primary"
                 href={data?.src}>
                 Download AEP(X)
-            </Button>
+              </Button>
               <Button
                 disabled={isLoading}
                 style={{ margin: 10, marginLeft: 0 }}
                 variant="contained"
                 color="primary"
                 onClick={handleDownload}>
-                {isLoading ? 'Preparing...' : 'Download template with assets'}
+                {isLoading ? "Preparing..." : "Download template with assets"}
               </Button>
               <Button
                 style={{ margin: 10 }}
@@ -227,44 +277,52 @@ export default () => {
                 color="secondary"
                 onClick={handleDelete}>
                 {isDeleting ? "Deleting..." : "Delete"}
-              </Button></RoleBasedView>
-            <RoleBasedView allowedRoles={['Admin']}>
-              {(data?.publishState === 'pending' || data?.publishState === 'rejected') && <Button
-                disabled={isLoading}
-                style={{ margin: 10, marginLeft: 0 }}
-                variant="contained"
-                color="primary"
-                onClick={() => handleUpdateTemplate("published")
-                }
-                children="Approve"
-              />}{(data?.publishState !== 'unpublished') && <Button
-                disabled={data?.publishState === 'rejected' || isLoading}
-                style={{ margin: 10, marginLeft: 0 }}
-                variant="contained"
-                color="secondary"
-                onClick={() => setIsDialogOpen(true)}
-                children={data?.publishState === 'rejected' ? "Rejected" : 'Reject'}
-              />}
+              </Button>
+            </RoleBasedView>
+            <RoleBasedView allowedRoles={["Admin"]}>
+              {(data?.publishState === "pending" ||
+                data?.publishState === "rejected") && (
+                <Button
+                  disabled={isLoading}
+                  style={{ margin: 10, marginLeft: 0 }}
+                  variant="contained"
+                  color="primary"
+                  onClick={() => handleUpdateTemplate("published")}
+                  children="Approve"
+                />
+              )}
+              {data?.publishState !== "unpublished" && (
+                <Button
+                  disabled={data?.publishState === "rejected" || isLoading}
+                  style={{ margin: 10, marginLeft: 0 }}
+                  variant="contained"
+                  color="secondary"
+                  onClick={() => setIsDialogOpen(true)}
+                  children={
+                    data?.publishState === "rejected" ? "Rejected" : "Reject"
+                  }
+                />
+              )}
               <RejectionReasonPrompt
                 open={isDialogOpen}
                 onClose={() => setIsDialogOpen(false)}
                 onSubmit={({ rejectionReason }) => {
-                  handleUpdateTemplate("rejected", rejectionReason)
-                  setIsDialogOpen(false)
+                  handleUpdateTemplate("rejected", rejectionReason);
+                  setIsDialogOpen(false);
                 }}
                 value={data?.rejectionReason}
-              /></RoleBasedView>
+              />
+            </RoleBasedView>
           </div>
         </Container>
       </Paper>
-    </div >
+    </div>
   );
 };
 
-
 const getRenderCostForTemplate = (time, value) => {
-  return ((time / 60000) * value)
-}
+  return (time / 60000) * value;
+};
 const getColorFromState = (state) => {
   switch (state) {
     case "rejected":
