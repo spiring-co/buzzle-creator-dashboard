a262de516f336593b59e88dc148eff7924f9990f
MadSadLad <shivam.119966@yahoo.com>
2021-01-19 13:37:14 +0530

    hard coded creator role

--- src/domains/Profile/CreatorProfile/index.js
+++ src/domains/Profile/CreatorProfile/index.js
@@ -11,7 +11,11 @@ import {
   MenuItem,
   Select,
   Input,
+  Modal,
+  Backdrop,
+  Fade,
 } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import Tab from "@material-ui/core/Tab";
 import Tabs from "@material-ui/core/Tabs";
 import { Alert } from "@material-ui/lab";
@@ -25,8 +29,23 @@ import { useAuth } from "services/auth";
 import VerticalTabs from "common/VerticalTabs";
 import { Prompt } from "react-router-dom";
 import ChangePassword from "domains/Auth/ChangePassword";
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 function ProfileEdit({ creator }) {
-  console.log("creator is:" + creator.imageUrl);
+  console.log("creator is:" + JSON.stringify(creator));
   const [isBlocking, setIsBlocking] = useState(true);
 
   const [loading, setLoading] = useState(false);
@@ -183,15 +202,144 @@ function Setting() {
   );
 }
 
+function Webhooks() {
+  const [webhook, setWebhook] = useState("");
+  const [webhookData, setWebhookData] = useState([]);
+  const [url, setUrl] = useState("");
+  const { user } = useAuth();
+  const [open, setOpen] = useState(false);
+  const classes = useStyles();
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    console.log(JSON.stringify(user));
+    fetch("http://localhost:5000/webhooks/")
+      .then((response) => response.json())
+      .then((data) => setWebhookData(data));
+
+    fetch(`http://localhost:5000/users/${user?.id}`)
+      .then((response) => response.json())
+      .then((data) => setCurrentUser(data));
+  }, []);
+
+  useEffect(() => {
+    console.log(webhookData);
+  }, [webhookData]);
+
+  useEffect(() => {
+    console.log(currentUser);
+  }, [currentUser]);
+
+  const handleChange = (event) => {
+    setWebhook(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    console.log(user?.id);
+    fetch(`http://localhost:5000/users/${user?.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Im9XSXRWSE5xOCIsImVtYWlsIjoic2hpdmFtLjExOTk2NkB5YWhvby5jb20iLCJuYW1lIjoic2hpdmFtIHR5YWdpIiwicm9sZSI6IkFkbWluIiwiaW1hZ2VVcmwiOiJodHRwczovL2ltYWdlcy51bnNwbGFzaC5jb20vcGhvdG8tMTYwMDYwNDQ3NzM3MS03ZjJkZGY3ODJhMmI_aXhsaWI9cmItMS4yLjEmYXV0bz1mb3JtYXQmZml0PWNyb3Amdz02MTkmcT04MCIsImlhdCI6MTYxMDk2NjcwOCwiZXhwIjoxNjEzNTU4NzA4fQ.ZG5E2d9tc6C2JqT3DnqpxfPyGmQVEixsOUYeLTatUbY",
+      },
+      body: JSON.stringify({
+        webhooks: { id: webhook.id, url: url, name: webhook.name },
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  return (
+    <Container style={{ display: "flex", flexDirection: "column" }}>
+      <Typography variant="h5">Webhook</Typography>
+      <Divider />
+      {currentUser
+        ? currentUser?.webhooks.map((cu) => {
+            return (
+              <div>
+                <Typography style={{ margin: 10 }} variant="h7">
+                  Name: {cu.name}
+                </Typography>
+                <Typography variant="h7">URL: {cu.url}</Typography>
+              </div>
+            );
+          })
+        : " "}
+      <Button
+        style={{ marginTop: 20 }}
+        size="small"
+        type="button"
+        onClick={handleOpen}
+        variant="contained">
+        Add a Webhook
+      </Button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        className={classes.modal}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}>
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <Container style={{ padding: 35 }}>
+              <Select
+                labelId="demo-simple-select-helper-label"
+                id="demo-simple-select"
+                value={webhook}
+                style={{ width: 150 }}
+                onChange={handleChange}>
+                {webhookData.map((w) => {
+                  return <MenuItem value={w}>{w.name}</MenuItem>;
+                })}
+              </Select>
+            </Container>
+            <TextField
+              name="url"
+              key="random1"
+              variant="outlined"
+              onChange={(event) => {
+                setUrl(event.target.value);
+              }}
+              value={url}
+              label="URL"></TextField>
+            <Button
+              onClick={handleSubmit}
+              style={{ marginTop: 10 }}
+              variant="contained">
+              Set
+            </Button>
+          </div>
+        </Fade>
+      </Modal>
+    </Container>
+  );
+}
+
 export default () => {
-  const [creator, setCreator] = useState({});
-  const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
   const { user } = useAuth();
-  const [webhook, setWebhook] = useState("");
-  const [webhookData, setWebhookData] = useState("");
-  const [url, setUrl] = useState("");
-
+  const [creator, setCreator] = useState({});
+  const [error, setError] = useState(null);
   useEffect(() => {
     Creator.get(user?.id)
       .then((c) => {
@@ -200,59 +348,9 @@ export default () => {
       .catch(setError)
       .finally(() => setLoading(false));
   }, []);
-
-  useEffect(() => {
-    fetch("http://localhost:5000/webhooks/")
-      .then((response) => response.json())
-      .then((data) => setWebhookData(data));
-  }, []);
-
-  useEffect(() => {
-    console.log(webhookData);
-  }, [webhookData]);
-
   if (loading) {
     return <Typography>loading...</Typography>;
   }
-  const handleChange = (event) => {
-    setWebhook(event.target.value);
-  };
-  const handleChangeUrl = (event) => {
-    setUrl(event.target.value);
-  };
-
-  function Webhooks() {
-    return (
-      <Container style={{ display: "flex", flexDirection: "column" }}>
-        <Typography variant="h5">Webhook</Typography>
-        <Divider />
-        <Container style={{ padding: 35 }}>
-          <Select
-            labelId="demo-simple-select-helper-label"
-            id="demo-simple-select"
-            value={webhook}
-            style={{ width: 150 }}
-            onChange={handleChangeUrl}>
-            {webhookData.map((w) => {
-              return <MenuItem value={w.id}>{w.name}</MenuItem>;
-            })}
-            {/* <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem> */}
-          </Select>
-        </Container>
-        <TextField
-          name="url"
-          variant="outlined"
-          onChange={handleChange}
-          value={url}
-          label="URL"></TextField>
-        <Button style={{ marginTop: 10 }} variant="contained">
-          Set
-        </Button>
-      </Container>
-    );
-  }
-
   return (
     <Container>
       {error && <Alert severity="error">{error.message}</Alert>}
@@ -282,7 +380,7 @@ export default () => {
           {
             label: "Webhooks",
             component: <Webhooks />,
-            allowedRoles: ["Creator", "User"],
+            allowedRoles: ["Admin", "Creator", "User"],
           },
         ]}
       />
