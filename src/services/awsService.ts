530adef94f16063409736aaa969a14cded2a6dcd
Abdul Basit <abasit325@gmail.com>
2022-05-24 19:04:20 +0530

    fix upload

--- src/services/awsService.ts
+++ src/services/awsService.ts
@@ -1,6 +1,6 @@
-import AWS from "aws-sdk";
+import AWS, { S3 } from "aws-sdk";
 
-const bucketName = "spiring-creator";
+const bucketName = "buzzle";
 const bucketRegion = "us-east-1";
 const tagsByUseCase = {
   archive: "true",
@@ -8,13 +8,13 @@ const tagsByUseCase = {
   deleteAfter90Days: "90 Days",
 };
 
-AWS.config.update({
-  region: bucketRegion,
-});
+// AWS.config.update({
+//   region: bucketRegion,
+// });
 
-AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-  IdentityPoolId: "us-east-1:06431ed0-60d3-457c-9c6c-7866955fc5e5",
-});
+// AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+//   IdentityPoolId: "us-east-1:06431ed0-60d3-457c-9c6c-7866955fc5e5",
+// });
 // Create EC2 service object
 var ec2 = new AWS.EC2({ apiVersion: "2016-11-15" });
 
@@ -22,19 +22,23 @@ var ec2 = new AWS.EC2({ apiVersion: "2016-11-15" });
  * @param  {String} Key Path of the file
  * @param  {} Body File body
  */
-export const upload = (Key: string, Body: any, tag?: "archive" | "deleteAfter7Days" | "deleteAfter90Days") => {
-  var upload = new AWS.S3.ManagedUpload({
-    partSize: 15 * 1024 * 1024,
-    queueSize: 5,
-    params: {
-      Bucket: bucketName,
-      Key: Key,
-      Body,
-      ACL: "public-read",
-    },
-    tags: [{ Key: (tag || "archive"), Value: tagsByUseCase[tag || "archive"] }],
-  });
+const s3Client = new S3({
+  endpoint: "https://sgp1.digitaloceanspaces.com",
+  region: bucketRegion,
+  credentials: {
+    accessKeyId: "RFQVO4EOPKFXOVLJQJYL",
+    secretAccessKey: "x6ZaZ39bCBEpiq8XfImx2rDBZarPc18Gp9sEZHULr9E"
+  }
+});
 
+export const upload = (Key: string, Body: any, tag?: "archive" | "deleteAfter7Days" | "deleteAfter90Days") => {
+
+  var upload = s3Client.upload({
+    Bucket: bucketName,
+    Key: tag === 'deleteAfter90Days' ? `${tag}/${Key}` : Key,
+    Body,
+    ACL: 'public-read',
+  });
   return upload;
 
   /*
@@ -54,3 +58,5 @@ export const getInstanceInfo = async () => {
   console.log(response);
   return response;
 };
+
+
