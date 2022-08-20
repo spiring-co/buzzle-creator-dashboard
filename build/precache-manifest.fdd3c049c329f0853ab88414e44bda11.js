d2cac3f596191eaff70ae04e9ce02228b3a21b56
Abdul Basit <abasit325@gmail.com>
2022-08-20 21:31:48 +0530

    added build directory

---
+++ build/precache-manifest.fdd3c049c329f0853ab88414e44bda11.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd80f0f946eb80ac024fb7d712f650a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e3037581eb8bea4540b",
+    "url": "/static/css/2.043008e4.chunk.css"
+  },
+  {
+    "revision": "5e3037581eb8bea4540b",
+    "url": "/static/js/2.a236d837.chunk.js"
+  },
+  {
+    "revision": "16ea9e367cb7c46df6df93476e762fd2",
+    "url": "/static/js/2.a236d837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02ac5d3ad1aca92bfcd7",
+    "url": "/static/js/3.40874bcf.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/3.40874bcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a810e46c50691792434",
+    "url": "/static/js/4.6142b86c.chunk.js"
+  },
+  {
+    "revision": "312b03fc2032f4a26db5e85de50ce98a",
+    "url": "/static/js/4.6142b86c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2c9c31c205bc0857735",
+    "url": "/static/js/5.03177f54.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.03177f54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79d2768b6b4471930dfd",
+    "url": "/static/js/main.99bbf11b.chunk.js"
+  },
+  {
+    "revision": "06ad8bc30c65f10756f4",
+    "url": "/static/js/runtime-main.8940eee4.js"
+  },
+  {
+    "revision": "f4c82894038cbf7ae9359693265b732a",
+    "url": "/static/media/404.f4c82894.svg"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/static/media/getFetch.013f0b90.cjs"
+  },
+  {
+    "revision": "2b9b4872cd25494093c1eb14f0264a0b",
+    "url": "/static/media/jsoneditor-icons.2b9b4872.svg"
+  },
+  {
+    "revision": "1e388c68c590f80e7686fc32bfa82d63",
+    "url": "/static/media/landing.1e388c68.svg"
+  },
+  {
+    "revision": "5a649ff921f37aa905722c664cf762e5",
+    "url": "/static/media/spiring.5a649ff9.svg"
+  },
+  {
+    "revision": "4eaa8e789fda31fa262d0e43a80edbb3",
+    "url": "/static/media/spiring_grey.4eaa8e78.svg"
+  },
+  {
+    "revision": "7d7a15b28fc898e4c2b7a825b30f5983",
+    "url": "/static/media/video.7d7a15b2.svg"
+  }
+]);
